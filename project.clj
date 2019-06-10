37b4760bb12ddfc4f7b556e4a9f30d7a3a268e2e
Ygor Sad <ysads2010@gmail.com>
2019-06-10 12:43:53 -0300

    [ci] configure travis

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject monstera "0.0.1-SNAPSHOT"
   :description "A survival guide for people in love with plants and pets"
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+                   :plugins [[lein-midje "3.2.1"]]}
              :midje {}})
