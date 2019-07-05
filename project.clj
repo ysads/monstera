5d09ea3c3ee4a888c3cbee82a097c43111b3f364
Ygor Sad <ysads2010@gmail.com>
2019-07-05 15:19:57 -0300

    [deps] add reaver parser

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject monstera "0.0.1-SNAPSHOT"
   :description "A survival guide for people in love with plants and pets"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[aleph "0.4.6"]
+                 [org.clojure/clojure "1.7.0"]
+                 [reaver "0.1.2"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.2.1"]]}
              :midje {}})
